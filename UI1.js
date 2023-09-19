843fa3bbf3cb8eebf974be60d0926a6cdb576b0b
InsaneX4 <dibyansu.mishra2003@gmail.com>
2023-09-19 23:25:28 +0530

    updates

--- UI1.js
+++ UI1.js
@@ -5,77 +5,64 @@ document.addEventListener('DOMContentLoaded', () => {
   const regis = document.querySelector('#signbtn');
   const regiscontainer = document.querySelector('.Pop-up-regis');
   const signin = document.querySelector('#signinbtn');
-  const navregis = document.getElementById('registernav');
-  const navlogin = document.getElementById('loginnav');
-  const loginTextData = [
-    { en: 'Login', hi: 'लॉग इन' },
-];
+  const navregis = document.querySelector('#registernav');
+  const navlogin = document.querySelector('#loginnav');
+  loginButton.addEventListener('click', () => {
+    loginContainer.classList.add('active');
+  });
+  background.addEventListener('click', () => {
+      console.log('Background clicked');
+      loginContainer.classList.remove('active');
+      regiscontainer.classList.remove('active');
+    });
 
-const registerTextData = [
-    { en: 'Register', hi: 'पंजीकरण' },
-];
+      regis.addEventListener('click', () => {
+      console.log('Background clicked');
+      loginContainer.classList.remove('active');
+      regiscontainer.classList.add('active');
 
-let loginIndex = 0;
-let registerIndex = 0;
+    });
+    navregis.addEventListener('click', () => {
+      console.log('Background clicked');
+      loginContainer.classList.remove('active');
+      regiscontainer.classList.add('active');
 
-function toggleLanguage() {
-    const currentLoginText = loginTextData[loginIndex];
-    const currentRegisterText = registerTextData[registerIndex];
+    });
+    navlogin.addEventListener('click', () => {
+      console.log('Background clicked');
+      loginContainer.classList.add('active');
+      regiscontainer.classList.remove('active');
 
-    navlogin.textContent = currentLoginText.en;
-    navregis.textContent = currentRegisterText.en;
+    });
+      signin.addEventListener('click', () => {
+          regiscontainer.classList.remove('active');
+          loginContainer.classList.add('active');
+      });
+  
+      const initialVideo = document.getElementById("background-video");
 
-    setTimeout(() => {
-        navlogin.textContent = currentLoginText.hi;
-        navregis.textContent = currentRegisterText.hi;
-    }, 1500); // Switch to Hindi after 1.5 seconds
-
-    loginIndex = (loginIndex + 1) % loginTextData.length;
-    registerIndex = (registerIndex + 1) % registerTextData.length;
-}
-
-// Toggle the language for Login and Register links every 3 seconds (3000 milliseconds)
-setInterval(toggleLanguage, 3000);
-
-// Toggle the language for Login and Register links separately every 3 seconds (3000 milliseconds)
-setInterval(toggleLoginLanguage, 3000);
-setInterval(toggleRegisterLanguage, 3000);
-
+      // Function to crossfade between the initial and looped videos
+      function crossfadeVideos() {
+          // Crossfade duration in milliseconds (adjust as needed)
+          const crossfadeDuration = 1000; // 1 second
+          
+          // Set the transition for the initial video
+          initialVideo.style.transition = `opacity ${crossfadeDuration}ms`;
+      
+          // Fade out the initial video
+          initialVideo.style.opacity = 0;
+      
+          // When the initial video fades out, reset its opacity and play it again
+          setTimeout(() => {
+              initialVideo.style.opacity = 1;
+              initialVideo.play();
+          }, crossfadeDuration);
+      }
+      
+      // Play the initial video
+      //initialVideo.play();
+      
+     
 
   
-loginButton.addEventListener('click', () => {
-  console.log('Login button clicked');
-  loginContainer.classList.add('active');
 });
-
-background.addEventListener('click', () => {
-  console.log('Background clicked');
-  loginContainer.classList.remove('active');
-  regiscontainer.classList.remove('active');
-});
-
-regis.addEventListener('click', () => {
-  console.log('Register button clicked');
-  loginContainer.classList.remove('active');
-  regiscontainer.classList.add('active');
-});
-
-navregis.addEventListener('click', () => {
-  console.log('Register link clicked');
-  loginContainer.classList.remove('active');
-  regiscontainer.classList.add('active');
-});
-
-navlogin.addEventListener('click', () => {
-  console.log('Login link clicked');
-  loginContainer.classList.add('active');
-  regiscontainer.classList.remove('active');
-});
-
-signin.addEventListener('click', () => {
-  regiscontainer.classList.remove('active');
-  loginContainer.classList.add('active');
-});
-
-});
-  
